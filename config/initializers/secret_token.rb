2b3d65e60953287f99691e3fd1ef4a7485d41609
Carsten Ringe <carsten.ringe@web.de>
2011-09-27 17:49:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'a70cb0df3ebf5147d7bb7258fe6ca1e7271b11ad1dc367a26ce432d375885d04186693d2624803954cd857264762d87eb81d948bf92017e3fc186229c784b3b7'
---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+Product.delete_all
+Product.create(:title => 'Product #1', :description => %{This is a simple product.}, :image_url => '/ruby.jpg', :price => 123.45)
+Product.create(:title => 'Product #2', :description => %{This is a simple product.}, :image_url => '/ruby2.jpg', :price => 123.45)
