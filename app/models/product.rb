867ad65a753c8799767fea566ac5e0ce8617fd26
Carsten Ringe <carsten.ringe@web.de>
2011-09-27 18:21:47 +0200

    Add more tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, :presence => true
+  validates :title, :uniqueness => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :image_url, :allow_blank => false, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'Must be of type .gif/.jpg/.png'
+  }
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def new_product(image_url)
+    Product.new(:title => 'Product', :description => %{A description}, :image_url => image_url, :price => 0.01)
+  end
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?, product.errors[:title].join('; ')
+    assert product.errors[:description].any?, product.errors[:description].join('; ')
+    assert product.errors[:image_url].any?, product.errors[:image_url].join('; ')
+    assert product.errors[:price].any?, product.errors[:price].join('; ')
+  end
+
+  test "product has unique title" do
+    product = Product.new(:title => products(:ruby).title, :description => %{short description}, :image_url => 'ruby.jpg', :price => 0.01)
+    assert !product.save
+  end
+
+  test "price must be positive" do
+    product = new_product '/ruby.jpg'
+    product.price = 0.00
+    assert product.invalid?
+    assert product.errors[:price].any?
+    product.price = -1
+    assert product.invalid?
+    assert product.errors[:price].any?
+    product.price = 0.01
+    assert product.valid?
+    assert product.errors[:price].none?
+  end
+
+  test "image url has valid extension" do
+    ok = %w{ ruby.png /ruby.jpg http://local/test.gif }
+    bad = %w{ false.doc http://a/b/c.png/more c.png.exe }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
 end
