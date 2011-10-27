466d5979c9cb08933ef6b2f61c2d78dce90f370f
Carsten Ringe <carsten.ringe@web.de>
2011-10-27 19:45:38 +0200

    Add a cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, :presence => true
   validates :title, :uniqueness => true, :length => {
     :minimum => 10
@@ -8,4 +12,14 @@ class Product < ActiveRecord::Base
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'Must be of type .gif/.jpg/.png'
   }
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
